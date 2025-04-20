667c3d8a2bec3ecb9e985b352abc50b6852a3e80
Adeelaman <66818277+Adeelaman@users.noreply.github.com>
2025-04-21 04:55:36 +0500

    Update job-card-wide.blade.php

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -2,14 +2,19 @@
 
 <x-panel class="flex gap-x-6">
     <div>
-        <x-employer-logo />
+        <x-employer-logo :employer="$job->employer" />
     </div>
 
     <div class="flex-1 flex flex-col">
-        <a href="" class="self-start text-sm text-gray-400">{{ $job->employer->name }}</a>
+        <a href="#" class="self-start text-sm text-gray-400 transition-colors duration-300">{{ $job->employer->name }}</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">{{ $job->title }}</h3>
-        <p class="text-sm text-gray-400 mt-auto">{{ $job->schedule }} - From {{ $job->salary }}</p>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800">
+            <a href="{{ $job->url }}" target="_blank">
+                {{ $job->title }}
+            </a>
+        </h3>
+
+        <p class="text-sm text-gray-400 mt-auto">{{ $job->salary }}</p>
     </div>
 
     <div>
@@ -18,4 +23,3 @@
         @endforeach
     </div>
 </x-panel>
-
