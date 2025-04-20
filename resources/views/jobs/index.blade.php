763398527de8b97604c96190f1edac926be62733
Adeelaman <66818277+Adeelaman@users.noreply.github.com>
2025-04-21 04:55:29 +0500

    Update index.blade.php

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,14 +1,14 @@
 <x-layout>
     <div class="space-y-10">
-        <section class="text-center">
-            <h1 class="font-bold text-4xl">Let's find you a Job.</h1>
+        <section class="text-center pt-6">
+            <h1 class="font-bold text-4xl">Let's Find Your Next Job</h1>
 
-            <form action="" class="mt-6">
-                <input type="text" name="" placeholder="Manager..." id="" class="bg-white/5 border-white/25 px-5 py-4 rounded-xl w-full max-w-xl">
-            </form>
+            <x-forms.form action="/search" class="mt-6">
+                <x-forms.input :label="false" name="q" placeholder="Web Developer..." />
+            </x-forms.form>
         </section>
 
-        <section class="pt-6">
+        <section class="pt-10">
             <x-section-heading>Featured Jobs</x-section-heading>
 
             <div class="grid lg:grid-cols-3 gap-8 mt-6">
@@ -30,12 +30,12 @@
 
         <section>
             <x-section-heading>Recent Jobs</x-section-heading>
-        </section>
 
-        <div class="mt-6 space-y-6">
-            @foreach($jobs as $job)
-                <x-job-card-wide :$job />
-            @endforeach
-        </div>
+            <div class="mt-6 space-y-6">
+                @foreach($jobs as $job)
+                    <x-job-card-wide :$job />
+                @endforeach
+            </div>
+        </section>
     </div>
 </x-layout>
